c5809d8c98ff583c1ad6e3a805382a93fe6d0081
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2026-02-26 14:40:57 +0100

    Update cw_1.py

--- cw_1.py
+++ cw_1.py
@@ -1,1 +1,55 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
+# Listy
+# 1
+# a = [3, 7, 2, 9, 4]
+# b =[a[1],a[2]]
+# print (b)
+
+# 2
+# b = ["Jan", "Anna", "Piotr"]
+# b[0] = 'Zuzanna'
+# print (b)
+
+# 3
+# c = [1, 2, 3]
+# c.append(4)
+# print(c)
+
+# 4
+# d = [10, 20, 30, 40, 50]
+# d2 = d[::2]
+# print(d2)
+
+# 5
+# f = ["1", "2", "3"]
+# f2 =list(map(int,f))
+# print(sum(f2))
+
+# Słowniki
+# 6
+# person = {"name":"Ewa", "age":28}
+# person.update({"city":"Olsztyn"})
+# print(person)
+
+# 7
+# counts = {"x":5, "y":0}
+# counts["x"] += 3
+# print(counts)
+
+# 8
+rows = [{"name":"A","val":10}, {"name":"B","val":20}]
+rows
+# 9
+# info = {"k":"v", "m":None}
+# del info["m"]
+# print(info)
+
+# 10
+sales = {
+    "apple": 10,
+    "banana": 5,
+    "orange": 8
+}
