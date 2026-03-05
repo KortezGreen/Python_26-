2ceb6e3ffa5fa5e5853232bae48a7d9034beef8f
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2026-03-05 14:30:06 +0100

    Update cw_2.py

--- cw_2.py
+++ cw_2.py
@@ -2,3 +2,58 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+# zad 1
+# a = np.arange(2, 42, 2)
+# print(a)
+# print(type(a))
+
+# zad 2
+# l = (1.3,4.2,4.55)
+# print(l)
+# a = np.array(l, dtype='int64')
+# print(a)
+
+# zad 3
+# n = int(input("Podaj liczbę całkowitą: "))
+# def tab(n):
+#     t = np.arange(1, n**2 + 1).reshape(n, n)
+#     return t
+# print('Tablica: \n',tab(n))
+# print('Ma wymiar: ',tab(n).shape)
+
+# zad 4
+# a = int(input("Podaj liczbę całkowitą a: "))
+# b = int(input("Podaj liczbę całkowitą b: "))
+# def generuj(a,b):
+#     w = np.logspace(1,b,num=b, base=a)
+#     return w
+# print('Wynik: \n', generuj(a,b))
+
+# zad 5
+# d = int(input("Podaj liczbę całkowitą a: "))
+# def mac(d):
+#     w = np.arange(1, d + 1)[::-1]
+#     m = np.diag(w)
+#     return m
+# print(mac(d))
+
+# zad 6-
+
+
+# zad 7
+n = int(input("Podaj liczbę całkowitą: "))
+def tab(n):
+    t = np.arange(1).reshape(n, n)
+    dd = np.fill_diagonal(t, 5)
+    return dd
+print('Tablica: \n',tab(n))
+
+# zad 8
+
+
+# zad 9
+
